ba1fadde8924b127f88a2632088b79e5fbdbbc8e
yukio-sato <yukioutiyamasato@gmail.com>
2024-08-29 19:22:59 -0300

    [29/08/2024] QR Generator Working

--- js/qrCodeGen.js
+++ js/qrCodeGen.js
@@ -1,6 +1,7 @@
     const { jsPDF } = window.jspdf;
     const pdf = new jsPDF();
     async function down(nm,enter,userInfo,x,y){
+        // Biblioteca = https://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html
         //pdf.text(`test:`, 10, 10); // text on pdf
     
         /* other method to generate a qr code with jspdf api
@@ -8,13 +9,15 @@
         await QRCode.toCanvas(qrCodeCanvas, "https://api.qrserver.com/v1/create-qr-code/?data=${teste}&size=100x100");
         const qrCodeDataUrl = qrCodeCanvas.toDataURL('image/png');
         */
-        let xBase = 66;
-        let yBase = 27;
-        let x2 = 32 +(xBase*(x-1))
-        let y2 = 18+(yBase*(y-1))
-        pdf.addImage(`https://api.qrserver.com/v1/create-qr-code/?data=${userInfo}&size=100x100`, 'PNG', 10+(xBase*(x-1)), 10+(yBase*(y-1)), 20, 20); // the qr code itself
-    
-        pdf.text(`Nome: ${nm.substring(0,1).toUpperCase()+nm.substring(1)}`, x2, y2); // text on pdf
+        let xBase = 67;
+        let yBase = 25;
+        let x2 = 32+(xBase*(x-1))
+        let y2 = 20+(yBase*(y-1))
+        pdf.addImage(`https://api.qrserver.com/v1/create-qr-code/?data=${userInfo}&size=100x100`, 'PNG', 10+(xBase*(x-1)), 12+(yBase*(y-1)), 20, 20); // the qr code itself
+
+        pdf.setFontSize(10);
+
+        pdf.text(`${nm.substring(0,1).toUpperCase()+nm.substring(1,19)}`, x2, y2); // text on pdf
         if (enter == "CON"){
             pdf.text(`Convidado`, x2, y2+5); // text on pdf
         }
